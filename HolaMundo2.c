6bcbbc01daefbf108bb7c8f51372d174f353089f
armandovaz <49042456+armandovaz@users.noreply.github.com>
2020-10-07 16:53:26 -0500

    Coloqué mi nombre

--- HolaMundo2.c
+++ HolaMundo2.c
@@ -4,5 +4,6 @@ printf("\nFernando Julian Diaz Vazquez\n");
 printf("Este es mi segundo codigo");
 printf("Programar es facil");
 printf("Voy a compartir con mi compañero el codigo");
+printf("\nArmando Vázquez\n");
 return 0;
 }
